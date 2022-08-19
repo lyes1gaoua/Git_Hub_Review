e1b4b1b86423681b26d5d185aff1237d6a6598d3
lyes1gaoua <lyes1gaoua@gmail.com>
2022-08-18 21:48:07 -0400

    changes

---
+++ ebay/src/main/java/HomePage.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class HomePage {
+//    WebDriver driver ;
+//    @BeforeMethod
+//    public void connectToWebDriver(){
+//        driver = new ChromeDriver();
+//        System.setProperty("webDriver.chrome.driver", "C:\\Users\\13477\\IdeaProjects\\ebay\\src\\main\\webDriver\\chromedriver.exe");
+//        driver.get("http: // www.ebay.com/");
+//        driver.manage().window().maximize();
+//        driver.manage().deleteAllCookies();
+//        driver.manage().timeouts()  .implicitlyWait(5, TimeUnit.SECONDS) ;
+//
+//
+//    }
+//    @AfterMethod
+//    public void closeBrowser() throws InterruptedException {
+//        Thread.sleep(3000);
+//        driver.close();
+//    }
+
+}
